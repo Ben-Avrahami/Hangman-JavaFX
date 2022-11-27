87e5014d8f1241160fb29bd81394f392dc2f0731
Ben Avrahami <benben733@gmail.com>
2022-11-28 01:59:23 +0200

    added changes to fxml, changed lines and circle opacity and added shapes array

--- src/HangmanController.java
+++ src/HangmanController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Shape;
 
 public class HangmanController {
 
@@ -48,35 +49,47 @@ public class HangmanController {
 	public final int NUMBER_OF_LETTERS=26;
 	public final int BUTTONS_IN_A_ROW=7;
 	private Button[] btns;
+	private Shape[] shapes;
+	private final int NUMBER_OF_STRIKES=10;
+	private int strikes=0;
 
 	private char letter='a';
 
 	public void initialize() {
-		btns = new Button[NUMBER_OF_LETTERS];		
+		btns = new Button[NUMBER_OF_LETTERS];
+		Shape shapes[]={firstLine,secondLine,thirdLine,forthLine,fifthLine,sixthLine,seventhLine,eightLine,ninthLine,tenthLine}; 
+		shapes[3].setVisible(true);
 		for (int i = 0; i < btns.length; i++) {
 			btns[i] = new Button(letter+ "");
 			btns[i].setPrefSize(btnGrid.getPrefWidth()/BUTTONS_IN_A_ROW, btnGrid.getPrefHeight()/BUTTONS_IN_A_ROW);
 			btnGrid.add(btns[i], i%BUTTONS_IN_A_ROW , i/BUTTONS_IN_A_ROW);
 			letter++;
-			
+
 			btns[i].setOnAction(new EventHandler<ActionEvent>(){
-				
+
 				public void handle(ActionEvent event) {
 					handleBtn(event);
 				}
-			
-				
+
+
+
 			});
-		
+
 		}
 	}
-		
-		private void handleBtn(ActionEvent event) {
-			System.out.println("test");
+
+	private void handleBtn(ActionEvent event) {
+		Button btn=(Button) event.getSource();
+		if(strikes<NUMBER_OF_STRIKES) {
+			System.out.println(btn.getText());
+			btn.setDisable(true);
 		}
-
-
-
+		else 
+			System.out.println("game is over");
 	}
+
+
+
+}
 
 
