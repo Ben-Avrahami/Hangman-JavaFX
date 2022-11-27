a9980667b6bbc8a2bc690c661678573491eb2fc4
Ben Avrahami <benben733@gmail.com>
2022-11-28 00:44:33 +0200

    added fxml and java initial files

---
+++ src/HangmanController.java
@@ -0,0 +1,67 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
+public class HangmanController {
+
+	@FXML
+	private GridPane btnGrid;
+
+	@FXML
+	private Line eightLine;
+
+	@FXML
+	private Circle fifthLine;
+
+	@FXML
+	private Line firstLine;
+
+	@FXML
+	private Line forthLine;
+
+	@FXML
+	private Line ninthLine;
+
+	@FXML
+	private Line secondLine;
+
+	@FXML
+	private Line seventhLine;
+
+	@FXML
+	private Line sixthLine;
+
+	@FXML
+	private Line tenthLine;
+
+	@FXML
+	private Label textBox;
+
+	@FXML
+	private Line thirdLine;
+
+	public final int NUMBER_OF_LETTERS=26;
+	public final int BUTTONS_IN_A_ROW=7;
+	private Button[] btns;
+
+	private char letter='a';
+
+	public void initiallize() {
+		btns = new Button[NUMBER_OF_LETTERS];
+		 btnGrid.add(new Button(), 1, 0);
+		
+		for (int i = 0; i < btns.length; i++) {
+			btns[i] = new Button(letter+ "");
+			btns[i].setPrefSize(btnGrid.getPrefWidth()/BUTTONS_IN_A_ROW, btnGrid.getPrefHeight()/BUTTONS_IN_A_ROW);
+			btnGrid.add(btns[i], i%BUTTONS_IN_A_ROW , i/BUTTONS_IN_A_ROW);
+			letter++;
+		}
+
+
+
+	}
+}
+
