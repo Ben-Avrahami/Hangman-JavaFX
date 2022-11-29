fead6011f9ac57518bb524dc95cd1d35fd0c3b0d
Ben Avrahami <benben733@gmail.com>
2022-11-29 19:01:43 +0200

    before taking functions out of the initiallize

--- src/HangmanController.java
+++ src/HangmanController.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -49,68 +51,100 @@ public class HangmanController {
 	public final int NUMBER_OF_LETTERS=26;
 	public final int BUTTONS_IN_A_ROW=7;
 	private Button[] btns;
-	private Shape[] shapes;
 	private final int NUMBER_OF_STRIKES=10;
+	private Shape[] shapes;//shapes array
 	private int strikes=0;
-	private String labelText="_ _ _ _ _ _ _ _ _ _ _";
+	private String labelText="";
 	private char letter='a';
+	int result=-1;
 	HangmanLogic game;
 
 	public void initialize() {
-		HangmanLogic game= new HangmanLogic("letters");
-		btns = new Button[NUMBER_OF_LETTERS];
-		Shape shapes[]={firstLine,secondLine,thirdLine,forthLine,fifthLine,sixthLine,seventhLine,eightLine,ninthLine,tenthLine};
-		textBox.setText(labelText);//sets the inital text of the label
-		shapes[3].setVisible(true);//set visibility of the shape 
-		for (int i = 0; i < btns.length; i++) {
-			btns[i] = new Button(letter+ "");
-			btns[i].setPrefSize(btnGrid.getPrefWidth()/BUTTONS_IN_A_ROW, btnGrid.getPrefHeight()/BUTTONS_IN_A_ROW);
-			btnGrid.add(btns[i], i%BUTTONS_IN_A_ROW , i/BUTTONS_IN_A_ROW);
-			letter++;
+		HangmanLogic game= new HangmanLogic("armadilo");//creates the game
+		btns = new Button[NUMBER_OF_LETTERS];//array of buttons
+		shapes=new Shape[NUMBER_OF_STRIKES];
+		shapes[0]=firstLine;
+		shapes[1]=secondLine;
+		shapes[2]=thirdLine;
+		shapes[3]=forthLine;
+		shapes[4]=fifthLine;
+		shapes[5]=sixthLine;
+		shapes[6]=seventhLine;
+		shapes[7]=eightLine;
+		shapes[8]=ninthLine;
+		shapes[9]=tenthLine;//shapes array
+		setInitialLabelText(game.getWord().length());//sets the inital text of the label to be _ at the length of the word
+		for (int i = 0; i < btns.length; i++) {//creates the buttons from a-z and attaches a function to the buttons
+			btns[i] = new Button(letter+ "");//adds a text to the button
+			btns[i].setPrefSize(btnGrid.getPrefWidth()/BUTTONS_IN_A_ROW, btnGrid.getPrefHeight()/BUTTONS_IN_A_ROW);//sets the size to fit the window
+			btnGrid.add(btns[i], i%BUTTONS_IN_A_ROW , i/BUTTONS_IN_A_ROW);//adds the button to the grid
+			letter++;//goes to the next char
 
 			btns[i].setOnAction(new EventHandler<ActionEvent>(){
 
 				public void handle(ActionEvent event) {
-					handleBtn(event);
-				}
+					result=0;
+					Button btn=(Button) event.getSource();//gets the button the was pressed
+					btn.setDisable(true);
+					if(strikes<NUMBER_OF_STRIKES) {//if the game is still continuing
+						char c= btn.getText().charAt(0);
+						result=game.checkLetter(c,0);
+						if(result==-1) {
+							//strike();//adds a strike
+							game.addStrike();
+							shapes[game.getStrikes()-1].setVisible(true);//set visibility of the shapes
+						
+							if (game.getStrikes()==NUMBER_OF_STRIKES) {
+								int answer = JOptionPane.showOptionDialog(null, "Gameover, do you want to play another game?","Hangman", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null , null , 0);
+								if(answer!=0)
+									return;//stops the game from running
+								//else {
+								//create a replay function
+								//}
+							}
+							return;//stops the running of the functions for this button press if it was not correct
+						}
+						setLabelText(result, c);
+						int last= game.getWord().lastIndexOf(c);
+						while(result!=last) {
+							result=game.checkLetter(c,result+1);
+							setLabelText(result, c);
+						}
 
-
-
-			});
-
-		}
-	}
-
-	private void handleBtn(ActionEvent event) {
-		Button btn=(Button) event.getSource();
-		if(strikes<NUMBER_OF_STRIKES) {
-			System.out.println(btn.getText());
-			char c= btn.getText().charAt(0);
-			int result=game.checkLetter(c,0);
-			System.out.println(btn.getText());
-
-			int last= game.getWord().lastIndexOf(c);
-			if (result!= last)
-			{
-				setLabelText(result, c);
-				result=game.checkLetter(c,result+1);
-			}
-			btn.setDisable(true);
+					}
 			
-			
-		}
-		else 
-			System.out.println("game is over");
+				else 
+					System.out.println("game is over");
+			}	
+		});
+
+
 	}
-	
-	private void setLabelText(int i, char c) {
-		i=i*2;//becuase of the whitespaces
-		char[] textArray=labelText.toCharArray();
-		textArray[i]=c;
-		labelText=String.valueOf(textArray);
-		textBox.setText(labelText);
-		
+
+}
+
+private void setLabelText(int i, char c) {
+	char[] textArray=labelText.toCharArray();
+	textArray[i]=c;
+	labelText=String.valueOf(textArray);
+	textBox.setText(labelText);
+
+}
+/**
+ * This method sets the initial text of the label 
+ * @param length int representing the length of the word the user need to guess correctly in the hangman game
+ */
+private void setInitialLabelText(int length) {
+	for (int i=0; i<length; i++) {
+		labelText= labelText+ "_";
+
 	}
+	textBox.setText(labelText);
+}
+private void strike() {
+	game.addStrike();
+	shapes[game.getStrikes()-1].setVisible(true);//set visibility of the shape
+}
 
 
 
--- src/HangmanLogic.java
+++ src/HangmanLogic.java
@@ -1,19 +1,28 @@
 
 public class HangmanLogic {
-	private int strikes=0;
+	private int _strikes=0;
 	private String _word; 
 	
 public HangmanLogic(String word) {
-	_word=word;
+	_word=word.toLowerCase();
+	_strikes=0;
+	
 }
 
 public int checkLetter(char letter, int from) {
+	if(from <= _word.length())
+	System.out.println("here2");
 	return _word.indexOf(letter,from);//returns the index of the given letter or -1 if not exists
 }
+
 public String getWord() {
 	return _word;
 }
+
 public void addStrike() {
-	strikes++;
+	_strikes++;
+}
+public int getStrikes() {
+	return _strikes;
 }
 }
